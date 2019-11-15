f4e0fe04f4ff8187a8064f46b6f0ce3e61af9527
Glitch (hello-webpage) <none>
2019-11-15 21:57:06 +0000

    🎉🐣 Checkpoint ./index.html:1732569/31 ./style.css:1732569/2 ./script.js:1732569/1597

--- script.js
+++ script.js
@@ -2,4 +2,31 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("hi");
+const regExpElement = document.querySelector('textarea[name=regexp]');
+const testStringElement = document.querySelector('textarea[name=test-string]');
+const previewElement = document.getElementById('preview');
+
+const render = () => {
+  const regexp = new RegExp(regExpElement.innerHTML.trim());
+  console.log(regexp);
+  const testString = testStringElement.innerHTML;
+  const match = testString.match(regexp);
+  console.log('match:', match);
+  const index = regexp.search(testString);
+  previewElement.innerHTML = match;
+}
+
+regExpElement.addEventListener('change', () => {
+  render();
+});
+regExpElement.addEventListener('keyup', () => {
+  render();
+});
+testStringElement.addEventListener('change', () => {
+  render();
+});
+testStringElement.addEventListener('keyup', () => {
+  render();
+});
+
+render();
