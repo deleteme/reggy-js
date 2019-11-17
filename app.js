b4c67d943634785a2fa5d622761480e0ba54cf48
Glitch (hello-webpage) <none>
2019-11-17 01:46:34 +0000

    💫👹 Checkpoint ./app.js:1732569/9282

--- app.js
+++ app.js
@@ -2,29 +2,68 @@ import {
   render,
   html
 } from "https://cdn.jsdelivr.net/npm/lit-html@1.1.2/lit-html.js";
-import { getState, setState } from "./store.js";
+import { getState, setState as _setState } from "./store.js";
+
+const preview = (state) => {
+  try {
+    const regexp = new RegExp(state.regExpString);
+    console.log(regexp);
+    const testString = testStringElement.value;
+    const match = testString.match(regexp);
+    console.log("match:", match);
+    if (match) {
+      const formatted = testString.replace(
+        [...new Set(match)],
+        `<span class='match'>$&</span>`
+      );
+
+      previewElement.innerHTML = formatted;
+    } else {
+      previewElement.innerHTML = testString;
+    }
+    document.body.classList.toggle("has-error", false);
+  } catch (error) {
+    document.body.classList.toggle("has-error", true);
+    previewElement.innerHTML = `<span class="syntax-error">${error}</span>`;
+  }
+
+  return html`
+
+  `;
+}
 
 const app = state => {
-  const handleChange = e => {
-    const { value, name } = 
-  }
+  const handler = e => {
+    const { value, name } = e.target;
+    setState({ [name]: value });
+  };
   return html`
     <div class="panels">
       <section class="panel regexp-panel">
         <label>Regular Expression</label>
-        <textarea name="regexp" class="field" @change=${
-  e => {
-    console.log('change',e );
-  }
-}>${state.regExpString}</textarea>
+        <textarea
+          name="regExpString"
+          class="field"
+          @keydown=${handler}
+          @change=${handler}
+        >
+${state.regExpString}</textarea
+        >
       </section>
       <section class="panel">
         <label>Test String</label>
-        <textarea name="test-string" class="field">${state.testString}</textarea>
+        <textarea
+          name="testString"
+          class="field"
+          @keydown=${handler}
+          @change=${handler}
+        >
+${state.testString}</textarea
+        >
       </section>
       <section class="panel">
         <label>Preview</label>
-        <pre class="preview"></pre>
+        <pre class="preview">${preview(state)}</pre>
       </section>
     </div>
   `;
@@ -33,6 +72,11 @@ const app = state => {
 const renderApp = () => {
   const state = getState();
   render(app(state), document.getElementById("app"));
+};
+
+const setState = newState => {
+  _setState(newState);
+  renderApp();
 };
 
 renderApp();
