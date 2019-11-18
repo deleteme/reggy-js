d5b09973819928b3e3da0048c087d714469c4c17
Glitch (hello-webpage) <none>
2019-11-18 02:08:31 +0000

    ⛹👟 Checkpoint ./style.css:1732569/338 ./templates/test-string-panel.js:1732569/350

--- templates/test-string-panel.js
+++ templates/test-string-panel.js
@@ -3,22 +3,24 @@ import { preview } from "./preview.js";
 
 export const testStringPanel = ({ state, setState, handler }) => {
   return html`
-    <section class="panel">
-      <label class="label" for="test-string">Test String</label>
-      <textarea
-        name="testString"
-        id="test-string"
-        class="textarea"
-        @keyup=${handler}
-        @change=${handler}
-        spellcheck="false"
-      >
-${state.testString}</textarea
-      >
-    </section>
-    <section class="panel">
-      <label class="label">Preview</label>
-      <pre class="preview">${preview({ state })}</pre>
+    <section class="panel test-string-panel-input-mask">
+      <section class="panel test-string-panel">
+        <label class="label" for="test-string">Test String</label>
+        <textarea
+          name="testString"
+          id="test-string"
+          class="textarea"
+          @keyup=${handler}
+          @change=${handler}
+          spellcheck="false"
+        >
+  ${state.testString}</textarea
+        >
+      </section>
+      <section class="panel preview-panel">
+        <label class="label">Preview</label>
+        <pre class="preview">${preview({ state })}</pre>
+      </section>
     </section>
   `;
 };
