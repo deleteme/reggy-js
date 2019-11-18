2cc13ef760eae5b263b5a83c9672fa13a9ccc488
Glitch (hello-webpage) <none>
2019-11-18 01:56:29 +0000

    🍏💅 Checkpoint ./templates/app.js:1732569/754 ./templates/test-string.js:1732569/550 ./templates/test-string-panel.js:1732569/2467

--- templates/app.js
+++ templates/app.js
@@ -1,7 +1,7 @@
 import { html } from "../packages.js";
 import { getRegExp } from "../selectors.js";
-import { preview } from "./preview.js";
 import { regExpPanel } from "./regexp-panel.js";
+import { testStringPanel } from "./test-string-panel.js";
 import { classMap } from '../lib/class-map.js';
 
 export const app = ({ state, setState }) => {
@@ -13,27 +13,14 @@ export const app = ({ state, setState }) => {
   return html`
     <div
       class="${
-        classMap({ panels: true, "has-error": regExp instanceof Error })
+        classMap({
+          panels: true,
+          "has-error": regExp instanceof Error
+        })
       }"
     >
       ${regExpPanel({ state, setState, handler })}
-      <section class="panel">
-        <label class="label" for="test-string">Test String</label>
-        <textarea
-          name="testString"
-          id="test-string"
-          class="textarea"
-          @keyup=${handler}
-          @change=${handler}
-          spellcheck="false"
-        >
-${state.testString}</textarea
-        >
-      </section>
-      <section class="panel">
-        <label class="label">Preview</label>
-        <pre class="preview">${preview({ state })}</pre>
-      </section>
+      ${testStringPanel({ state, setState, handler })}
     </div>
   `;
 };
---
+++ templates/test-string-panel.js
@@ -0,0 +1,24 @@
+import { html, repeat } from "../packages.js";
+import { preview } from "./preview.js";
+
+export const testStringPanel = ({ state, setState, handler }) => {
+  return html`
+    <section class="panel">
+      <label class="label" for="test-string">Test String</label>
+      <textarea
+        name="testString"
+        id="test-string"
+        class="textarea"
+        @keyup=${handler}
+        @change=${handler}
+        spellcheck="false"
+      >
+${state.testString}</textarea
+      >
+    </section>
+    <section class="panel">
+      <label class="label">Preview</label>
+      <pre class="preview">${preview({ state })}</pre>
+    </section>
+  `;
+};
