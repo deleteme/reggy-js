324dfbd135b13f0b4c2e134b0e97008b01448e39
Matt Garrett <mattga@gmail.com>
2019-11-22 22:10:01 -0800

    add measurement code

--- app.js
+++ app.js
@@ -2,8 +2,30 @@ import { render } from "./packages.js";
 import { subscribe } from "./store.js";
 import { app } from "./templates/app.js";
 
+const add = (a, b) => a + b;
+
+const sum = values => {
+  return values.reduce(add, 0);
+};
+
+const average = values => {
+  const total = sum(values);
+  return Math.round(total / values.length);
+};
+
+const measure = () => {
+  const end = Date.now();
+  const duration = end - window.start;
+  const durations = JSON.parse(localStorage.getItem('durations')) || [];
+  durations.push(duration);
+  console.log('durations.length', durations.length, 'average', average(durations));
+  console.log('durations', durations);
+  localStorage.setItem('durations', JSON.stringify(durations));
+}
+
 const renderApp = ({ state, setState }) => {
   render(app({ state, setState }), document.getElementById("app"));
+  measure();
 };
 
 subscribe(renderApp);
