fd53e78aefe401b4738611567fdf8d8be67010ea
Matt Garrett <mattga@gmail.com>
2020-01-20 11:09:48 -0800

    fix iOS test string preview

--- templates/test-string-panel.js
+++ templates/test-string-panel.js
@@ -5,14 +5,22 @@ import { getMatch } from "../selectors.js";
 const renderMatchCount = ({ state }) => {
   const match = getMatch(state);
   return match
-    ? html`<small class="match-length">${match.length} ${
-      match.length === 1 ? 'match' : 'matches'
-      }</small>`
-    : '';
-}
+    ? html`
+        <small class="match-length"
+          >${match.length} ${match.length === 1 ? "match" : "matches"}</small
+        >
+      `
+    : "";
+};
 
 export const testStringPanel = ({ state, setState, handler }) => {
-  
+  const handleTestString = ({ target }) => {
+    requestAnimationFrame(() => {
+      // the event target value is set
+      // by the time this is called
+      handler({ target });
+    });
+  };
   return html`
     <section class="panel test-string-panel-input-mask">
       <section class="panel test-string-panel">
@@ -20,22 +28,17 @@ export const testStringPanel = ({ state, setState, handler }) => {
           <label class="label" for="test-string">
             Test String
           </label>
-          ${ renderMatchCount({ state }) }
+          ${renderMatchCount({ state })}
         </header>
         <textarea
           name="testString"
           id="test-string"
           class="textarea"
-          @keydown=${({ target }) => {
-            requestAnimationFrame(() => {
-              // the event target value is set
-              // by the time this is called
-              handler({ target });
-            });
-          }}
-          @change=${handler}
+          @input=${handleTestString}
           spellcheck="false"
-        >${state.testString}</textarea>
+        >
+${state.testString}</textarea
+        >
       </section>
       <section class="panel preview-panel">
         <label class="label">Preview</label>
